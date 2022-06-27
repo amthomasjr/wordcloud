49f2f4f38c151762367f14403a08ef131675d031
Anthony M. Thomas, Jr <amthomasjr@gmail.com>
2022-06-26 19:40:59 -0500

    read contents of text file

--- wordcloud.py
+++ wordcloud.py
@@ -1,1 +1,5 @@
+# %% read contents of text file into variable named file_contents
+with open('the_gift_of_black_folk.txt', mode = 'r', encoding = 'utf-8-sig') as f:
+    file_contents = f.read()
 
+# %%
